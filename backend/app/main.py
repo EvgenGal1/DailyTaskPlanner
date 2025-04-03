0afa4f688876fb07cc21e0510e3a8d84a1b198e4
EvGen Gal <ZBst.1@yandex.ru>
2025-04-03 23:53:59 +0300

    SRV. # main + CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.config import settings
 
 app = FastAPI(title=settings.PROJECT_NAME)
 
+# настр.CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# нач.путь
 @app.get("/")
 def root():
     return {"message": "Добро пожаловать в DayTaskPlan!"}
